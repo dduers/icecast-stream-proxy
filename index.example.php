b40a658e543d0dc1cd8f250d399ad402c89c09ff
Daniel Duersteler <daniel.duersteler@gmail.com>
2020-01-08 22:34:49 +0100

    updated index.example.php

--- index.example.php
+++ index.example.php
@@ -11,7 +11,7 @@ $streamproxy = new icecastStreamProxy(array(
      * ogg and mp3 mounts
      * if a mp3 mount is given, it will fall back to it for microsoft browsers
      */
-	'ic_mount_ogg' => '/stream.ogg',
+    'ic_mount_ogg' => '/stream.ogg',
     'ic_mount_mp3' => '/stream.mp3',
     
     /**
@@ -26,7 +26,7 @@ $streamproxy = new icecastStreamProxy(array(
      * note: the stats page and all streams must share the same credentials
      * note: your icecast server must be setup to supprt http basic auth
      */
-	'ic_basic_auth_username' => 'proxy',
+    'ic_basic_auth_username' => 'proxy',
     'ic_basic_auth_password' => 'proxy_hackme',
     
     /**
@@ -34,6 +34,6 @@ $streamproxy = new icecastStreamProxy(array(
      * if you give an url here, all stream and stats requests are
      * limited to this referer (uses http referer header)
      */
-	'sp_lock_http_referer' => 'https://www.my-cool-radio-station.org'
+    'sp_lock_http_referer' => 'https://www.my-cool-radio-station.org'
 ));
 $streamproxy->run();
